46eafd3113ddc413da646d732f13f2e2ab10e2f4
Dmitry-Co <sechko.dp@gmail.com>
2023-10-30 19:44:24 +0500

    feat: add presence validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,7 @@ class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
 
+  validates :body, presence: true
+
   scope :correct_answers, -> { joins(:answers).where(answers: { correct: true }) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
+  validates :email, presence: true, uniqueness: true
+
   scope :tests_by_level, ->(level) {
     joins(:tests)
       .where(tests: { level: level })

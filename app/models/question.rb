d93249502ab4c9ca4705325ecde6c1100796876e
Dmitry-Co <sechko.dp@gmail.com>
2023-10-30 20:34:19 +0500

    feat: add answers condition(1..4) for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :body, presence: true
+  validates :answers, length: { in: 1...5 }
 
   scope :correct_answers, -> { joins(:answers).where(answers: { correct: true }) }
 end

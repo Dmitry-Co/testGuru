67a317748612fc14d058a69f3f469a91d93f401b
Dmitry-Co <sechko.dp@gmail.com>
2023-10-30 10:52:46 +0500

    feat: add scope(levels difficulty)

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
   def self.names_by_category(category_name)
     joins(:category)
       .where(categories: { title: category_name })

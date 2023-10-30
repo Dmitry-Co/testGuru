7b255602adb6a6f71dbf76ab643ef7e520c9f7df
Dmitry-Co <sechko.dp@gmail.com>
2023-10-30 19:25:43 +0500

    feat: add email attribute

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2023_10_19_145205) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

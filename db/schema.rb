40c91fb5cb52e5d5be553fff158255bd26dcdf4d
Dmitry-Co <sechko.dp@gmail.com>
2023-10-31 13:45:32 +0500

    feat: add Email attribute to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_31_054726) do
+ActiveRecord::Schema.define(version: 2023_10_31_083851) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2023_10_31_054726) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"

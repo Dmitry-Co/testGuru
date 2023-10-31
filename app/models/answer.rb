ccf8a73b9857f914ba2e970413c409e2412b7264
Dmitry-Co <sechko.dp@gmail.com>
2023-10-31 19:55:28 +0500

    fix: add validation for 4 answers precense

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
+
+  validate :validate_answers_count, on: :create
+
+  private
+
+  def validate_answers_count
+    errors.add(:base, 'A question must have from 1 to 4 answers') if
+     question.answers.count > 4 || question.answers.count < 1
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,4 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :body, presence: true
-  validates :answers, length: { in: 1...5 }
-
 end

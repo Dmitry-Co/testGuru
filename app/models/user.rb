5ec4079e6079dc374186a5f391c384cbf07b6c29
Dmitry-Co <sechko.dp@gmail.com>
2023-10-31 14:22:35 +0500

    fix: change scope in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   scope :tests_by_level, ->(level) {
-    joins(:tests)
-      .where(tests: { level: level })
+    joins(:tests).merge(Test.where(level: level))
   }
 end

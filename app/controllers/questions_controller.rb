dc3bcf0f7117ce35ef6ec6b6e88095560e52fc2a
Dmitry-Co <sechko.dp@gmail.com>
2023-11-04 16:19:02 +0500

    feat: add create question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,24 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    question = @test.questions.new(question_params)
+
+    if question.save
+      redirect_to question_path(question)
+    else
+      render 'new'
+    end
+  end
+end
+
+private
+
+def question_params
+  params.require(:question).permit(:body)
 end

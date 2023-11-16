f494230d5d93dff3a9b5b0491fbb420e0544c3e1
Dmitry-Co <sechko.dp@gmail.com>
2023-11-16 19:20:33 +0500

    feat: add question views. change question controller. add question helper

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create new]
-  before_action :find_question, except: %i[index create new]
+  before_action :find_question, only: %i[show edit update destroy]
+   before_action :find_test, only: %i[new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -13,22 +13,34 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = @test.questions.new
+    @question = Question.new
   end
 
   def create
-    question = @test.questions.new(question_params)
+    @question = @test.questions.new(question_params)
 
-    if question.save
-      redirect_to question_path(question)
+    if @question.save
+      redirect_to test_path(@test)
     else
       render 'new'
     end
   end
 
-  def destroy
-    @question.destroy
+  def edit
+    @question = Question.find(params[:id])
   end
+
+  def update
+    if @question.update(question_params)
+      redirect_to test_path(@question.test_id)
+    else
+      render :edit
+    end
+  end
+    def destroy
+      @question.destroy
+      redirect_to test_path(@question.test_id)
+    end
 
   private
 
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,5 @@
 module QuestionsHelper
+  def question_header(question)
+    question.new_record? ? "Create new #{@test.title} Question" : "Edit #{question.test.title} Question"
+  end
 end

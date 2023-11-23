04d7ab2aa46d80678a869d29911c0720cfb9e09e
Dmitry-Co <sechko.dp@gmail.com>
2023-11-23 18:11:35 +0500

    feat: add test_passage model and controller. drop test_user table

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
-  belongs_to :category
+  has_many :test_passages
+  has_many :users, through: :test_passages
   has_many :questions, dependent: :destroy
-  belongs_to :author, class_name: 'User'
-  has_many :tests_users
-  has_many :users, through: :tests_users
+  belongs_to :category, optional: true
+  belongs_to :author, class_name: 'User', foreign_key: :user_id, optional: true
 
   validates :title, presence: true
   validates :level, presence: true, numericality: { only_integer: true, greater_than: 0 }
---
+++ app/models/test_passage.rb
@@ -0,0 +1,6 @@
+class TestPassage < ApplicationRecord
+  belongs_to :user
+  belongs_to :test
+  belongs_to :current_question
+  
+end

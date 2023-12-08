3e69df36a747b160708e1eede62022fba93caca7
Dmitry-Co <sechko.dp@gmail.com>
2023-12-08 14:49:49 +0500

    feat: add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,40 +2,19 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  attr_reader :password
-  attr_writer :password_confirmation
+  include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :user_id
 
-  validates :email, presence: true, uniqueness: true
+  has_secure_password
 
   def tests_by_level(level)
     tests.where(level: level)
   end
 
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
-
-private
-
-def digest(string)
-  Digest::SHA1.hexdigest(string)
-end
-
 end

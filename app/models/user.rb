8209ba8f721c22a8423bb159ea6201f0fffcef75
Dmitry-Co <sechko.dp@gmail.com>
2023-12-13 16:26:07 +0500

    feat: add email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
+  EMAIL = /\A\S+@[a-z]+\.[a-z]+\z/
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :user_id
+
+  validates :email, presence: true, format: { with: EMAIL }, uniqueness: true
 
   has_secure_password
 

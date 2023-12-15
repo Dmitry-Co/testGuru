80a9478f206f6a9071189f6ab83d324b59317f3c
Dmitry-Co <sechko.dp@gmail.com>
2023-12-15 12:18:18 +0500

    feat: add setup mailer

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,6 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_test, only: %i[show edit update destroy start]
-  before_action :find_user, only: :start
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -42,8 +41,8 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private

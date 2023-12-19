a3d84cdbf35573cceaf168fe179a34e08e3e6fba
Dmitry-Co <sechko.dp@gmail.com>
2023-12-19 15:19:20 +0500

    feat: add User flash registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   helper FooterHelper
 
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     if resource.admin?
       admin_tests_path
@@ -9,4 +12,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :first_name, :last_name])
+  end
 end

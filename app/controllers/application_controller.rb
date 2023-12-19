569ccda948c8190fdfd6a2132781afb6ea983d69
Dmitry-Co <sechko.dp@gmail.com>
2023-12-19 13:15:08 +0500

    feat: add admin redirection to itself

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   helper FooterHelper
 
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_tests_path
+    else
+      tests_path
+    end
+  end
+
 end

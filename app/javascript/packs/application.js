d32125e61ec0f28e73beeadddcb3c6af1f917b39
Dmitry-Co <sechko.dp@gmail.com>
2024-01-09 18:16:37 +0500

    feat: Bootstrap setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,15 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
 // app/javascript/packs/application.js
+
+// Импорт зависимостей
+import "@hotwired/turbo-rails";
+import "controllers";
 import 'bootstrap';
 import '../stylesheets/application.css';
- 
+import $ from 'jquery';
+import 'popper.js';
+
+// Указываем пути для стилей Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Импорт Bootstrap из npm
+import 'bootstrap/dist/js/bootstrap.bundle';
